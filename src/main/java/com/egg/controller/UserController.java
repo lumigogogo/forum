2ebd4033476b80c589e6256d9c8e7fd596653799
lumi <luj@spsp-it.com>
2018-03-22 23:29:28 +0800

    1.完成spring-mvc配置 2.完成Spring-MVC Spring Mybatis框架整合

---
+++ src/main/java/com/egg/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.egg.controller;
+
+import com.egg.entity.User;
+import com.egg.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping(value = "/user", method = RequestMethod.GET)
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    public String toIndex(HttpServletRequest request, Model model){
+        long userId = Long.parseLong(request.getParameter("id"));
+        User user = userService.getUserById(userId);
+        model.addAttribute("user", user);
+        return "showUser";
+    }
+}
